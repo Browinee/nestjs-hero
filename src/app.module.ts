b90717d667c83815dadae1424cecb8403cfe5e29
User <user@AdmindeMacBook-Air.local>
2023-01-12 10:58:35 +0800

    feat: add zod to validate env

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { z } from 'zod';
+
+const envFilePath = `.env.${process.env.NODE_ENV || 'development'}`;
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [envFilePath],
+      load: [],
+      validationSchema: {
+        DB: z.string(),
+        PORT: z.number().default(3005),
+        DB_HOST: z.string().url(),
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService); // 取得 ConfigService
+  const PORT = configService.get('PORT');
   app.setGlobalPrefix('api/v1');
-  await app.listen(3005);
+  await app.listen(PORT);
 }
 bootstrap();
