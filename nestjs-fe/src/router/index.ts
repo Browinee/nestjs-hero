95c688b478319b80756921c5fa157e90d486fbea
Browinee <nihilitypeo@gmail.com>
2023-07-06 19:14:51 +0800

    chore: remove fe

--- nestjs-fe/src/router/index.ts
+++
@@ -1,9 +0,0 @@
-import { RouteRecordRaw, createRouter, createWebHashHistory } from "vue-router";
-
-const routes = [] as RouteRecordRaw[];
-const router = createRouter({
-  history: createWebHashHistory(),
-  routes,
-});
-
-export default router;
